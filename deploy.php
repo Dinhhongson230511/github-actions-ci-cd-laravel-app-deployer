4f269e9244f46f8de351c8fbb0add59401c7d4ca
sondh <dinhhongson23596@gmail.com>
2024-02-27 15:39:54 +0700

    update

--- deploy.php
+++ deploy.php
@@ -5,6 +5,7 @@ require 'recipe/laravel.php';
 
 // Config
 set('repository', 'git@github.com:Dinhhongson230511/github-actions-ci-cd-laravel-app-deployer.git');
+set('project_path', '/var/www/html/laravel-app/github-actions-ci-cd-laravel-app-deployer');
 set('keep_releases', 2);
 add('shared_files', ['.env']);
 add('shared_dirs', []);
@@ -37,7 +38,7 @@ host('app.dev')
     ->set('server_name', 'app.dev - $APP_NAME')
     ->set('server_url', '$APP_URL')
     ->set('branch', 'master')
-    ->set('deploy_path', '/var/www/html/laravel-app/github-actions-ci-cd-laravel-app-deployer');
+    ->set('deploy_path', '{{ project_path }}');
 
 // Hooks customize task for laravel recipes
 after('deploy:update_code', 'npm:run');
